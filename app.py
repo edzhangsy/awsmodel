2fc3109fe0a7647d9683e38c22335e77e8112af2
ed-zhang <zsy9822@gmail.com>
2022-06-19 12:58:37 +0800

    edit app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import sys
+import json
 from car_detection import image_to_box
 '''
-event: {
+body: {
     "file_hash": "md5",
     "file_b64": "b64"
 }
 '''
 def handler(event, context):
-    file_hash = event['file_hash']
-    file_b64 = event['file_b64']
+    body = event["body"]
     try:
+        body = json.loads(body)
+        file_hash = body['file_hash']
+        file_b64 = body['file_b64']
         out_box_json = image_to_box(file_b64)
     except Exception:
         return {
-            'statusCode': 500
+            'statusCode': 500,
+            'message': "json or b64 error"
         }
 
     # TODO check file hash
--- builddocker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo docker build -t awsmodel .
-# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 123456789012.dkr.ecr.us-east-1.amazonaws.com
-# aws ecr create-repository --repository-name awsmodel --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
-sudo docker tag  awsmodel:latest 772639918187.dkr.ecr.us-east-1.amazonaws.com/awsmodel:latest
-sudo docker push 772639918187.dkr.ecr.us-east-1.amazonaws.com/awsmodel:latest
